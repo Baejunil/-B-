b84983950aee1e2b1a417e41608281125783203d
minsu <example@naver.com>
2025-01-14 11:03:21 +0900

    basic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:8081/api/hello")
+    axios.get("http://localhost:8080/api/hello")
       .then((response) => setMessage(response.data))
       .catch((error) => console.error("Error fetching message:", error));
   }, []);
