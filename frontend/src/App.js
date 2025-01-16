4c722a8522a5e9d0fe75e23d61fc53c8e49e8c39
민수 <mimimido12@naver.com>
2025-01-16 17:31:45 +0900

    수정 기능 까지 완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Diary from './component/Diary';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DiaryForm from './component/DiaryForm';
 import DiaryDetail from './component/DiaryDetail';
+import DiaryEdit from './component/DiaryEdit';
 
 function App() {
   
@@ -17,6 +18,7 @@ function App() {
         <Route path="/diary" element={<Diary />} />
         <Route path="/diary/create" element={<DiaryForm />} />
         <Route path="/diary/:id" element={<DiaryDetail />} />  
+        <Route path="/diary/edit/:id" element={<DiaryEdit />} />
       </Routes>
     </Router>
   );
